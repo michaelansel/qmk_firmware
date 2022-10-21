14daa0c3bdc3e561580a612b138702c2113e84f3
mransel <mransel@twitch.tv>
2022-10-21 08:51:11 -0400

    Clean up my keymap

--- keyboards/converter/usb_usb/keymaps/michaelansel/keymap.c
+++ keyboards/converter/usb_usb/keymaps/michaelansel/keymap.c
@@ -35,9 +35,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,         KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,
     KC_EQL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                                                      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                                                      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
-    KC_NO,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                                      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+    KC_NO,    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                                      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                                                      KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-              KC_LEFT_CTRL,     KC_LEFT_ALT,     KC_LEFT_GUI,     KC_LEFT_SHIFT,                                                 KC_RIGHT_SHIFT,    KC_RIGHT_GUI,  KC_RIGHT_ALT,  KC_RIGHT_CTRL,
+              KC_LCTL,  KC_LALT,  KC_LGUI,  KC_LSFT,                                                                             KC_RSFT,    KC_RGUI,  KC_RALT,  KC_RCTL,
                                                       MO(NAV),  KC_LALT,                                               KC_RGUI,  KC_RCTL,
                                                                 KC_HOME,                                               KC_PGUP,
                                             KC_BSPC,  KC_DEL,   KC_END,                                                KC_PGDN,  KC_ENTER, KC_SPC
@@ -46,10 +46,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [NAV] = LAYOUT_kinesis(
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,         KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,
     KC_EQL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                                                      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
-    KC_TAB,   KC_Q,     HYPR(KC_I),     KC_E,     HYPR(KC_A),     KC_T,                                                                      KC_BACKSPACE,     KC_U,     KC_TAB,     KC_O,     KC_ESCAPE,     KC_BSLS,
-    KC_NO,  KC_LEFT_CTRL,     KC_LEFT_ALT,     KC_LEFT_GUI,     KC_LEFT_SHIFT,      KC_G,                                                                      KC_LEFT,     KC_DOWN,     KC_UP,     KC_RIGHT,     KC_SCLN,  KC_QUOT,
-    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                                                      KC_HOME,     KC_PAGE_DOWN,     KC_PAGE_UP,  KC_END,   KC_SLSH,  KC_RSFT,
-              KC_GRV,   KC_NUBS,  KC_LEFT,  KC_RGHT,                                                                                       KC_UP,    KC_DOWN,  KC_LBRC,  KC_RBRC,
+    KC_TAB,   KC_Q,     HYPR(KC_I), KC_E,   HYPR(KC_A), KC_T,                                                                    KC_BSPC,  KC_U,     KC_TAB,   KC_O,     KC_ESC,   KC_BSLS,
+    KC_NO,    KC_LCTL,  KC_LALT,  KC_LGUI,  KC_LSFT,  KC_G,                                                                      KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_SCLN,  KC_QUOT,
+    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                                                      KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   KC_SLSH,  KC_RSFT,
+              KC_LCTL,  KC_LALT,  KC_LGUI,  KC_LSFT,                                                                             KC_RSFT,  KC_RGUI,  KC_RALT,  KC_RCTL,
                                                       MO(NAV),  KC_LALT,                                               KC_RGUI,  KC_RCTL,
                                                                 KC_HOME,                                               KC_PGUP,
                                             KC_BSPC,  KC_DEL,   KC_END,                                                KC_PGDN,  KC_ENTER, KC_SPC
