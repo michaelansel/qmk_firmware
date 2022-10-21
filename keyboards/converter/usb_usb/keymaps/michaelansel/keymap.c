c567dd9ee99f391f07612f26c9e454fb6600b658
mransel <mransel@twitch.tv>
2022-10-21 11:55:39 -0400

    Update my keymap

--- keyboards/converter/usb_usb/keymaps/michaelansel/keymap.c
+++ keyboards/converter/usb_usb/keymaps/michaelansel/keymap.c
@@ -1,7 +1,21 @@
 #include QMK_KEYBOARD_H
 
-#define NAV 2
+#define _LAYER_ KC_TRANSPARENT // Alias for the key that got us into this layer
 
+#define NAV 2
+#define SYM 3
+
+#define BSPC_SYM LT(SYM, KC_BSPC)
+
+#define H_A HYPR(KC_A) // warpd
+#define H_B HYPR(KC_B) // Launch Chrome
+#define H_C HYPR(KC_C) // Launch Meet
+#define H_D HYPR(KC_D) // Launch Typora
+#define H_E HYPR(KC_E) // Launch Slack
+#define H_F HYPR(KC_F) // Launch VS Code
+#define H_G HYPR(KC_G) // Previous Tab
+#define H_H HYPR(KC_H) // Next Tab
+#define H_I HYPR(KC_I) // Switch Monitor Focus
 
 /****************************************************************************************************
 *
@@ -40,19 +54,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
               KC_LCTL,  KC_LALT,  KC_LGUI,  KC_LSFT,                                                                             KC_RSFT,    KC_RGUI,  KC_RALT,  KC_RCTL,
                                                       MO(NAV),  KC_LALT,                                               KC_RGUI,  KC_RCTL,
                                                                 KC_HOME,                                               KC_PGUP,
-                                            KC_BSPC,  KC_DEL,   KC_END,                                                KC_PGDN,  KC_ENTER, KC_SPC
+                                            BSPC_SYM,  KC_DEL,   KC_END,                                                KC_PGDN,  KC_ENTER, KC_SPC
   ),
 
+/* TEMPLATE
+  [TEMP] = LAYOUT_kinesis(
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,         XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                           XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                           XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                           XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                           XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+               XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                                                 XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+                                                           XXXXXXX,   XXXXXXX,                                                     XXXXXXX,   XXXXXXX,
+                                                                      XXXXXXX,                                                     XXXXXXX,
+                                                XXXXXXX,   XXXXXXX,   XXXXXXX,                                                     XXXXXXX,   XXXXXXX,   XXXXXXX
+  ),
+*/
+
   [NAV] = LAYOUT_kinesis(
-    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,         KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,
-    KC_EQL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                                                      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
-    KC_TAB,   KC_Q,     HYPR(KC_I), KC_E,   HYPR(KC_A), KC_T,                                                                    KC_BSPC,  KC_U,     KC_TAB,   KC_O,     KC_ESC,   KC_BSLS,
-    KC_NO,    KC_LCTL,  KC_LALT,  KC_LGUI,  KC_LSFT,  KC_G,                                                                      KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_SCLN,  KC_QUOT,
-    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                                                      KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   KC_SLSH,  KC_RSFT,
-              KC_LCTL,  KC_LALT,  KC_LGUI,  KC_LSFT,                                                                             KC_RSFT,  KC_RGUI,  KC_RALT,  KC_RCTL,
-                                                      MO(NAV),  KC_LALT,                                               KC_RGUI,  KC_RCTL,
-                                                                KC_HOME,                                               KC_PGUP,
-                                            KC_BSPC,  KC_DEL,   KC_END,                                                KC_PGDN,  KC_ENTER, KC_SPC
-  )
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,         XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                           XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+    XXXXXXX,   XXXXXXX,   H_I,       KC_E,      H_A,       XXXXXXX,                                                                           KC_ESC,    H_G,       KC_TAB,    H_H,       KC_ESC,   XXXXXXX,
+    XXXXXXX,   KC_LCTL,   KC_LALT,   KC_LGUI,   KC_LSFT,   XXXXXXX,                                                                           KC_LEFT,   KC_DOWN,   KC_UP,     KC_RIGHT,  XXXXXXX,  XXXXXXX,
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                           KC_HOME,   KC_PGDN,   KC_PGUP,   KC_END,    XXXXXXX,  XXXXXXX,
+               KC_LCTL,   KC_LALT,   KC_LGUI,   KC_LSFT,                                                                                                 KC_RSFT,   KC_RGUI,   KC_RALT,   KC_RCTL,
+                                                           _LAYER_,  XXXXXXX,                                                      XXXXXXX,   XXXXXXX,
+                                                                     XXXXXXX,                                                      XXXXXXX,
+                                                XXXXXXX,   XXXXXXX,  XXXXXXX,                                                      XXXXXXX,   XXXXXXX,  XXXXXXX
+  ),
+
+    [SYM] = LAYOUT_kinesis(
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,         XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                           XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                           XXXXXXX,   KC_LCBR,   KC_TAB,    KC_RCBR,   XXXXXXX,   XXXXXXX,
+    XXXXXXX,   KC_LCTL,   KC_LALT,   KC_LGUI,   KC_LSFT,   XXXXXXX,                                                                           XXXXXXX,   KC_LPRN,   KC_MINUS,  KC_RPRN,   XXXXXXX,   XXXXXXX,
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                           KC_GRV,    KC_LBRC,   KC_UNDS,   KC_RBRC,   XXXXXXX,   XXXXXXX,
+               XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                                                 XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+                                                           XXXXXXX,   XXXXXXX,                                                     XXXXXXX,   XXXXXXX,
+                                                                      XXXXXXX,                                                     XXXXXXX,
+                                                _LAYER_,   XXXXXXX,   XXXXXXX,                                                     XXXXXXX,   XXXXXXX,   XXXXXXX
+  ),
 
 };
