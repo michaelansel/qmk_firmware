c11513761d82d0abd9c1eb82178d6959c5dd9ba1
mransel <mransel@twitch.tv>
2022-10-21 12:13:21 -0400

    Update my keymap

--- keyboards/converter/usb_usb/keymaps/michaelansel/keymap.c
+++ keyboards/converter/usb_usb/keymaps/michaelansel/keymap.c
@@ -2,20 +2,22 @@
 
 #define _LAYER_ KC_TRANSPARENT // Alias for the key that got us into this layer
 
+#define BASE 0
 #define NAV 2
 #define SYM 3
 
 #define BSPC_SYM LT(SYM, KC_BSPC)
 
-#define H_A HYPR(KC_A) // warpd
-#define H_B HYPR(KC_B) // Launch Chrome
-#define H_C HYPR(KC_C) // Launch Meet
-#define H_D HYPR(KC_D) // Launch Typora
-#define H_E HYPR(KC_E) // Launch Slack
-#define H_F HYPR(KC_F) // Launch VS Code
-#define H_G HYPR(KC_G) // Previous Tab
-#define H_H HYPR(KC_H) // Next Tab
-#define H_I HYPR(KC_I) // Switch Monitor Focus
+// Hammerspoon Triggers
+#define HS_WARP HYPR(KC_A) // Launch warpd
+#define HS_CHRM HYPR(KC_B) // Launch Chrome
+#define HS_MEET HYPR(KC_C) // Launch Meet
+#define HS_TYPR HYPR(KC_D) // Launch Typora
+#define HS_SLAK HYPR(KC_E) // Launch Slack
+#define HS_CODE HYPR(KC_F) // Launch VS Code
+#define HS_PTAB HYPR(KC_G) // Previous Tab
+#define HS_NTAB HYPR(KC_H) // Next Tab
+#define HS_NSCR HYPR(KC_I) // Switch Monitor Focus
 
 /****************************************************************************************************
 *
@@ -45,19 +47,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [0] = LAYOUT_kinesis(
-    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,         KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,
-    KC_EQL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                                                      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
-    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                                                      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
-    KC_NO,    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                                      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                                                      KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-              KC_LCTL,  KC_LALT,  KC_LGUI,  KC_LSFT,                                                                             KC_RSFT,    KC_RGUI,  KC_RALT,  KC_RCTL,
-                                                      MO(NAV),  KC_LALT,                                               KC_RGUI,  KC_RCTL,
-                                                                KC_HOME,                                               KC_PGUP,
-                                            BSPC_SYM,  KC_DEL,   KC_END,                                                KC_PGDN,  KC_ENTER, KC_SPC
-  ),
-
-/* TEMPLATE
+/* Layer TEMPLATE
   [TEMP] = LAYOUT_kinesis(
     XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,         XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
     XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                           XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
@@ -71,10 +61,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 */
 
+  [BASE] = LAYOUT_kinesis(
+    KC_ESC,    KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,           KC_F9,     KC_F10,    KC_F11,    KC_F12,    KC_PSCR,   KC_SCRL,   KC_PAUS,
+    KC_EQL,    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,                                                                              KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_MINS,
+    KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,                                                                              KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_BSLS,
+    KC_NO,     KC_A,      KC_S,      KC_D,      KC_F,      KC_G,                                                                              KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,   KC_QUOT,
+    KC_LSFT,   KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,                                                                              KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,   KC_RSFT,
+               KC_LCTL,   KC_LALT,   KC_LGUI,   KC_LSFT,                                                                                                 KC_RSFT,   KC_RGUI,   KC_RALT,   KC_RCTL,
+                                                           MO(NAV),   KC_LALT,                                                     KC_RGUI,   KC_RCTL,
+                                                                      KC_HOME,                                                     KC_PGUP,
+                                                BSPC_SYM,  KC_DEL,    KC_END,                                                      KC_PGDN,   KC_ENTER,  KC_SPC
+  ),
+
   [NAV] = LAYOUT_kinesis(
     XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,         XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
     XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                           XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
-    XXXXXXX,   XXXXXXX,   H_I,       KC_E,      H_A,       XXXXXXX,                                                                           KC_ESC,    H_G,       KC_TAB,    H_H,       KC_ESC,   XXXXXXX,
+    XXXXXXX,   XXXXXXX,   HS_NSCR,   XXXXXXX,   HS_WARP,   XXXXXXX,                                                                           KC_ESC,    HS_PTAB,   KC_TAB,    HS_NTAB,   KC_ESC,   XXXXXXX,
     XXXXXXX,   KC_LCTL,   KC_LALT,   KC_LGUI,   KC_LSFT,   XXXXXXX,                                                                           KC_LEFT,   KC_DOWN,   KC_UP,     KC_RIGHT,  XXXXXXX,  XXXXXXX,
     XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                           KC_HOME,   KC_PGDN,   KC_PGUP,   KC_END,    XXXXXXX,  XXXXXXX,
                KC_LCTL,   KC_LALT,   KC_LGUI,   KC_LSFT,                                                                                                 KC_RSFT,   KC_RGUI,   KC_RALT,   KC_RCTL,
@@ -83,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                 XXXXXXX,   XXXXXXX,  XXXXXXX,                                                      XXXXXXX,   XXXXXXX,  XXXXXXX
   ),
 
-    [SYM] = LAYOUT_kinesis(
+  [SYM] = LAYOUT_kinesis(
     XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,         XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
     XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                           XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
     XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                           XXXXXXX,   KC_LCBR,   KC_TAB,    KC_RCBR,   XXXXXXX,   XXXXXXX,
