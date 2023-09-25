1a786ec7c684b4d8aa4d2997ecdbc3b564de7f7b
Michael Ansel <git@anselcomputers.com>
2023-09-25 20:52:02 +0000

    Clean up numpad layer

--- keyboards/converter/usb_usb/keymaps/michaelansel/keymap.c
+++ keyboards/converter/usb_usb/keymaps/michaelansel/keymap.c
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                           XXXXXXX,   KC_7,      KC_8,      KC_9,      XXXXXXX,   XXXXXXX,
     XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                           XXXXXXX,   KC_4,      KC_5,      KC_6,      KC_MINUS,  XXXXXXX,
     XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                           XXXXXXX,   KC_1,      KC_2,      KC_3,      KC_SLSH,   XXXXXXX,
-               XXXXXXX,   XXXXXXX,   XXXXXXX,   _ENTRY_,                                                                                                 KC_0,      XXXXXXX,   KC_DOT,    XXXXXXX,
+               XXXXXXX,   XXXXXXX,   XXXXXXX,   _ENTRY_,                                                                                                 XXXXXXX,   XXXXXXX,   KC_DOT,    XXXXXXX,
                                                            XXXXXXX,   XXXXXXX,                                                     XXXXXXX,   XXXXXXX,
                                                                       XXXXXXX,                                                     XXXXXXX,
                                                 _ENTRY_,   XXXXXXX,   XXXXXXX,                                                     XXXXXXX,   XXXXXXX,   KC_0
