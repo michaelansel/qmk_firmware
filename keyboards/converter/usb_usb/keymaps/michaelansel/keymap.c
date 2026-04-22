a1d018e12ef90e7a2514acd273b54766e38aaf9b
Michael Ansel <git@anselcomputers.com>
2026-04-22 14:23:57 +0000

    Aerospace, base mods, old changes

--- keyboards/converter/usb_usb/keymaps/michaelansel/keymap.c
+++ keyboards/converter/usb_usb/keymaps/michaelansel/keymap.c
@@ -49,23 +49,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 */
 
+  // Plain character entry
   [BASE] = LAYOUT_kinesis(
     KC_ESC,    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,         MOD_FIX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   QK_BOOT,
     XXXXXXX,   KC_1,      KC_2,      KC_3,      KC_4,      KC_5,                                                                              KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_MINS,
     XXXXXXX,   KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,                                                                              KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_BSLS,
     XXXXXXX,   HR_A,      HR_S,      HR_D,      HR_F,      KC_G,                                                                              KC_H,      HR_J,      HR_K,      HR_L,      HR_SCLN,   KC_QUOT,
     XXXXXXX,   KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,                                                                              KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,   XXXXXXX,
-               DM_PLY1,   DM_PLY2,   XXXXXXX,   MO(NUM),                                                                                                 XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
-              //  KC_LCTL,   KC_LALT,   KC_LGUI,   KC_LSFT,                                                                                                 KC_RSFT,   KC_RGUI,   KC_RALT,   KC_RCTL,
-                                                           OSL(UNI),  HS_BTN1,                                                     HS_MCTR,   OSL(UNI),
-                                                                      HS_SCUP,                                                     XXXXXXX,
-                                                LTHMB1,    LTHMB2,    HS_SCDN,                                                     XXXXXXX,   KC_ENTER,  LT(SYM, KC_SPC)
+            //    DM_PLY1,   DM_PLY2,   XXXXXXX,   MO(NUM),                                                                                                 XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+               KC_LCTL,   KC_LALT,   KC_LGUI,   KC_LSFT,                                                                                                 KC_RSFT,   KC_RGUI,   KC_RALT,   KC_RCTL,
+                                                           MO(WIN),   XXXXXXX,                                                     XXXXXXX,   OSL(UNI),
+                                                                      XXXXXXX,                                                     XXXXXXX,
+                                                LTHMB1,    LTHMB2,    XXXXXXX,                                                     XXXXXXX,   KC_ENTER,  LT(SYM, KC_SPC)
   ),
 
+  // Secondary lmod: Cursor movement
   [NAV] = LAYOUT_kinesis(
     XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,         XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
     XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                           XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  XXXXXXX,
-    XXXXXXX,   XXXXXXX,   HS_SCRN,   XXXXXXX,   HS_WARP,   XXXXXXX,                                                                           XXXXXXX,   HS_PTAB,   XXXXXXX,   HS_NTAB,   XXXXXXX,  XXXXXXX,
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                           XXXXXXX,   HS_PTAB,   XXXXXXX,   HS_NTAB,   XXXXXXX,  XXXXXXX,
     XXXXXXX,   KC_LCTL,   KC_LALT,   KC_LGUI,   KC_LSFT,   XXXXXXX,                                                                           KC_LEFT,   KC_DOWN,   KC_UP,     KC_RIGHT,  XXXXXXX,  XXXXXXX,
     XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                           KC_HOME,   KC_PGDN,   KC_PGUP,   KC_END,    XXXXXXX,  XXXXXXX,
                XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                                                 HS_SCDN,   HS_SCUP,   HS_BTN1,   HS_MCTR,
@@ -74,10 +76,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                 XXXXXXX,   _ENTRY_,  XXXXXXX,                                                      XXXXXXX,   S(KC_ENT), XXXXXXX
   ),
 
+  // Primary lmod: Special character entry
   [SYM] = LAYOUT_kinesis(
     XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,         XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
     XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                           KC_CIRC,   KC_AMPR,   KC_ASTR,   XXXXXXX,   XXXXXXX,   XXXXXXX,
-    XXXXXXX,   KC_EXLM,   KC_AT,     KC_HASH,   KC_DLR,    KC_PERC,                                                                           KC_ESC,    KC_LCBR,   KC_TAB,    KC_RCBR,   KC_BSPC,   KC_DEL,
+    XXXXXXX,   KC_EXLM,   KC_AT,     KC_HASH,   KC_DLR,    KC_PERC,                                                                           KC_ESC,    KC_LCBR,   KC_TAB,    KC_RCBR,   XXXXXXX,   XXXXXXX,
     XXXXXXX,   KC_LCTL,   KC_LALT,   KC_LGUI,   KC_LSFT,   KC_EQL,                                                                            KC_PIPE,   KC_LPRN,   KC_MINUS,  KC_RPRN,   KC_COLN,   LSFT(KC_QUOT),
     XXXXXXX,   KC_BSLS,   KC_LT,     CPY_PST,   KC_GT,     KC_TILD,                                                                           KC_GRV,    KC_LBRC,   KC_UNDS,   KC_RBRC,   LSFT(KC_SLSH),   XXXXXXX,
                _______,   _______,   _______,   TG(NUM),                                                                                                 _______,   _______,   _______,   _______,
@@ -86,6 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                 _ENTRY_,   XXXXXXX,   XXXXXXX,                                                     XXXXXXX,   G(KC_ENT), KC_BSPC
   ),
 
+  // Numpad?
   [NUM] = LAYOUT_kinesis(
     XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,         XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
     XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                           XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
@@ -98,18 +102,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                 _ENTRY_,   XXXXXXX,   XXXXXXX,                                                     XXXXXXX,   XXXXXXX,   KC_0
   ),
 
+  // Ternary lmod: window layout navigation
+  [WIN] = LAYOUT_kinesis(
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,         XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                           XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   AE_FULL,                                                                           AE_MV_L,   AE_MV_D,   AE_MV_U,   AE_MV_R,   AE_TGFT,   XXXXXXX,
+    XXXXXXX,   OSM(MOD_LCTL),   OSM(MOD_LALT),   OSM(MOD_LGUI),   OSM(MOD_LSFT),   AE_EQLS,                                                                           AE_FO_L,   AE_FO_D,   AE_FO_U,   AE_FO_R,   AE_TGAC,   XXXXXXX,
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   AE_TRST,                                                                           AE_JO_L,   AE_JO_D,   AE_JO_U,   AE_JO_R,   AE_TGRT,   XXXXXXX,
+               XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                                                 XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+                                                           HS_MENU,   XXXXXXX,                                                     XXXXXXX,   XXXXXXX,
+                                                                      XXXXXXX,                                                     XXXXXXX,
+                                                XXXXXXX,   XXXXXXX,   XXXXXXX,                                                     XXXXXXX,   XXXXXXX,   XXXXXXX
+  ),
+
+  // Move app selections to a dedicated app layer separate from the Universal layer? Put mouse keys on the universal layer.
   [UNI] = LAYOUT_kinesis(
     XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,         XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
     XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                           XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
     XXXXXXX,   XXXXXXX,   XXXXXXX,   HS_MENU,   XXXXXXX,   XXXXXXX,                                                                           HS_CHRM,   HS_MEET,   HS_TYPR,   HS_SLAK,   HS_CODE,   HS_TERM,
     XXXXXXX,   OSM(MOD_LCTL),   OSM(MOD_LALT),   OSM(MOD_LGUI),   OSM(MOD_LSFT),   XXXXXXX,                                                                           RTM,    KC_MPRV,   KC_MPLY,   KC_MNXT,   HS_SPFY,   XXXXXXX,
-    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   C(KC_B),                                                                           XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
-               DM_REC1,   DM_REC2,   DM_RSTP,   XXXXXXX,                                                                                                 XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+    XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   C(KC_B),                                                                           XXXXXXX,   MAG_L,     MAG_MAX,   MAG_R,     MAG_RST,   XXXXXXX,
+               XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,                                                                                                 XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
+            //    DM_REC1,   DM_REC2,   DM_RSTP,   XXXXXXX,                                                                                                 XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,
                                                            HS_MENU,   XXXXXXX,                                                     XXXXXXX,   _ENTRY_,
                                                                       XXXXXXX,                                                     XXXXXXX,
                                                 XXXXXXX,   XXXXXXX,   XXXXXXX,                                                     XXXXXXX,   XXXXXXX,   XXXXXXX
   ),
 
-  // Move app selections do a dedicated app layer separate from the Universal layer? Put mouse keys on the universal layer.
 
 };
--- keyboards/converter/usb_usb/keymaps/michaelansel/michaelansel.h
+++ keyboards/converter/usb_usb/keymaps/michaelansel/michaelansel.h
@@ -6,6 +6,7 @@
 #define NAV 2 // Navigation/Focus control
 #define SYM 3 // Symbols
 #define UNI 4 // Universal/media keys
+#define WIN 5 // Window layout/navigation
 
 // Home row mods
 #ifdef HOME_ROW_MODS_ENABLE
@@ -47,6 +48,78 @@
 #define HS_MCTR HYPR(KC_P) // Move mouse to the center of the screen
 #define HS_MENU HYPR(KC_Q) // Open interactive hammerspoon menu system
 // TODO UNI-r left mouse click for selecting whatever window is already under the mouse (shortcut for single action in warpd)
+
+// Magnet.app
+#define MAG_L C(A(KC_LEFT))
+#define MAG_R C(A(KC_RIGHT))
+#define MAG_MAX C(A(KC_ENTER))
+#define MAG_RST C(A(KC_BACKSPACE))
+
+// Aerospace.app
+/*
+
+Two structural observations for layout design
+The shift convention.
+AeroSpace's default already establishes "unshifted = act on focus, shifted = act on window position."
+Preserving this (workspace 1 vs. send-window-to-workspace 1, focus-left vs. move-left) lets you halve the number of bindings you have to actively memorize — the second half is always just "the first half + shift."
+
+Directions want hjkl or arrows consistently.
+Focus, move, swap, resize, and join-with all take the same four-direction argument.
+If you use hjkl for focus but arrows for resize, you've doubled the cognitive load.
+Pick one direction convention and apply it everywhere it appears in the cluster map — the rest of the layout is modifier differentiation.
+
+*/
+/*
+# Workspace Navigation
+- Workspace next/prev
+  - Ctrl-L/R - reuse the standard Spaces keys
+
+# Focus
+- Focus LRUD
+  - Mod?? hjkl
+- Focus other monitor
+  - ?? replace the right thumb cmd key
+
+# Moving Windows
+- Send to workspace (and follow); direct, next/prev; other monitor
+- Move/Swap LRUD
+  - Mod?? yuio
+
+  # Layout
+  - Toggle float/tile
+  - Mod?? p
+  - Toggle tiles/accordion
+  - Mod?? ;
+  - Toggle horizontal/vertical
+  - Mod?? /
+  - Join LRUD
+  - Mod?? nm,.
+  - Mod?? sdef (kinesis wasd equiv)
+- Reset tree to flat
+  - Mod?? b
+- Equalize sizes
+  - Mod?? g
+- Fullscreen?
+  - Mod?? t
+*/
+#define AE_EQLS C(A(KC_EQL)) // balance-sizes
+#define AE_FULL C(A(S(KC_EQL))) // fullscreen
+#define AE_FO_D C(A(KC_J)) // focus down
+#define AE_FO_L C(A(KC_H)) // focus left
+#define AE_FO_R C(A(KC_L)) // focus right
+#define AE_FO_U C(A(KC_K)) // focus up
+#define AE_JO_D C(A(G(KC_J))) // join-with down
+#define AE_JO_L C(A(G(KC_H))) // join-with left
+#define AE_JO_R C(A(G(KC_L))) // join-with right
+#define AE_JO_U C(A(G(KC_K))) // join-with up
+#define AE_MV_D C(A(S(KC_J))) // move down
+#define AE_MV_L C(A(S(KC_H))) // move left
+#define AE_MV_R C(A(S(KC_L))) // move right
+#define AE_MV_U C(A(S(KC_K))) // move up
+#define AE_TGAC C(A(KC_COMM)) // layout tiles accordion (change container style)
+#define AE_TGFT C(A(KC_DOT)) // layout floating tiling
+#define AE_TGRT C(A(KC_SLSH)) // layout horizontal vertical (change container orientation)
+#define AE_TRST C(A(KC_QUOT)) // bash:make all workspace windows tiling, flatten-workspace-tree
 
 // Tap dances
 enum {
